f293b0134d7e5cb75bc206f44332aaae506e8703
HollyMollyHollyMolly <dnuyen9@academic.rrc.ca>
2025-09-24 21:41:22 -0500

    set up shape and trangle

--- shape/shape.py
+++ shape/shape.py
@@ -2,3 +2,32 @@
 
 __author__ = "Nguyen Dang Thai Ha"
 __version__ = "23/09/2025"
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    """This class represents a geometric shape."""
+    def __init__(self, color:str):
+        """Initialize the shape with a color."""
+        color = color.strip()
+    
+        if not color:
+            raise ValueError("Color must not be empty.")
+        self._color= color
+
+    def __str__(self) -> str:
+        """Return a string representation of the shape."""
+        return f"the shape color is (color={self._color})"
+    
+    @abstractmethod
+    def caculate_area(self) -> float:
+        """abstract method to Calculate the area of the shape."""
+        pass
+    
+    @abstractmethod
+    def caculate_perimeter(self) -> float:
+        """abstract method to Calculate the area of the shape."""
+        pass
+
+        
+    
--- shape/triangle.py
+++ shape/triangle.py
@@ -2,3 +2,48 @@
 
 __author__ = "Nguyen Dang Thai Ha"
 __version__ = "23/09/2025"
+
+from shape.shape import Shape
+import math
+
+class Triangle(Shape):
+    """This class represents a triangle shape."""
+    def __init__(self, color: str, side_1: int, side_2: int, side_3: int):
+        """Initialize the triangle with a color and three sides."""
+        super().__init__(color)
+
+
+        if not isinstance(side_1, int):
+            raise TypeError("Side 1 must be numeric.")
+
+        if not isinstance(side_2, int):
+            raise TypeError("Side 2 must be numeric.")  
+
+        if not isinstance(side_3, int):
+            raise TypeError("Side 3 must be numeric.")
+
+
+        if side_1 <= 0 or side_2 <= 0 or side_3 <= 0:
+            raise ValueError("Sides must be positive numbers.")
+        
+        if not ((side_1 + side_2 > side_3) and (side_1 + side_3 > side_2) and (side_2 + side_3 > side_1)):
+            raise ValueError("The sides do not satisfy the Triangle Inequality Theorem.")
+        
+        self.side_1 = side_1
+        self.side_2 = side_2
+        self.side_3 = side_3
+
+    def __str__(self) -> str:
+        """Return a string representation of the triangle."""
+        return f"the triangle color is (color={self._color}), This triangle has three sides with lengths of {self.side_1}, {self.side_2} and {self.side_3} centimeters.)"
+    
+    def caculate_area(self) -> float:
+        """Calculate the area of the triangle using Heron's formula. sp: semi-perimeter"""
+        sp = (self.side_1 + self.side_2 + self.side_3 )/ 2
+        area = math.sqrt ((sp * (sp - self.side_1) * (sp - self.side_2) * (sp - self.side_3))) 
+        return area
+    
+    def caculate_perimeter(self) -> float:
+        """Calculate the perimeter of the triangle."""
+        p = self.side_1 + self.side_2 + self.side_3 
+        return p
